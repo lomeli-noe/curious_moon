5abacb4970dc7aa276c81641ea09dfbad8cb31de
lomeli-noe <neolomeli@gmail.com>
2020-03-21 18:29:51 -0700

    import and normalize data

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,3 +1,10 @@
+drop table if exists events cascade;
+drop table if exists teams cascade;
+drop table if exists targets cascade;
+drop table if exists spass_types cascade;
+drop table if exists requests cascade;
+drop table if exists event_types cascade;
+
 create schema if not exists import;
 drop table if exists import.master_plan;
 create table import.master_plan(
@@ -11,4 +18,17 @@ request_name text,
 library_definition text,
 title text,
 description text
+);
+
+drop table if exists events;
+create table events(
+id serial primary key,
+time_stamp timestamptz not null,
+title varchar(500),
+description text,
+event_type_id int references event_types(id),
+target_id int references targets(id),
+team_id int references teams(id),
+request_id int references requests(id),
+spass_type_id int references spass_types(id)
 );
